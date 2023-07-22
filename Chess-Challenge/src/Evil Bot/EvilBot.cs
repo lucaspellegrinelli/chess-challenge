6d8b735765aee32f2200fa6621bfc058fa3a35cb
Lucas Pellegrinelli <lucas.r.pellegrinelli@gmail.com>
2023-07-22 15:37:02 -0300

    Tste

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -12,43 +12,114 @@ namespace ChessChallenge.Example
 
         public Move Think(Board board, Timer timer)
         {
+            bool areWeWhite = board.IsWhiteToMove;
             Move[] allMoves = board.GetLegalMoves();
 
             // Pick a random move to play if nothing better is found
             Random rng = new();
             Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-            int highestValueCapture = 0;
+            int bestScore = 0;
 
             foreach (Move move in allMoves)
             {
-                // Always play checkmate in one
-                if (MoveIsCheckmate(board, move))
+                board.MakeMove(move);
+
+                if (board.IsInCheckmate())
                 {
-                    moveToPlay = move;
-                    break;
+                    return move;
                 }
 
-                // Find highest value capture
-                Piece capturedPiece = board.GetPiece(move.TargetSquare);
-                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+                int boardScore = MinMax(board, 3, false, int.MinValue, int.MaxValue);
 
-                if (capturedPieceValue > highestValueCapture)
+                if (boardScore > bestScore)
                 {
                     moveToPlay = move;
-                    highestValueCapture = capturedPieceValue;
+                    bestScore = boardScore;
                 }
+
+                board.UndoMove(move);
             }
 
             return moveToPlay;
         }
 
-        // Test if this move gives checkmate
-        bool MoveIsCheckmate(Board board, Move move)
+        int MinMax(Board board, int depth, bool maximizingPlayer, float alpha, float beta)
         {
-            board.MakeMove(move);
-            bool isMate = board.IsInCheckmate();
-            board.UndoMove(move);
-            return isMate;
+            if (depth == 0)
+            {
+                return GetBoardScore(board) * (maximizingPlayer ? 1 : -1);
+            }
+
+            if (board.IsInCheckmate())
+            {
+                return maximizingPlayer ? int.MinValue : int.MaxValue;
+            }
+
+            Move[] allMoves = board.GetLegalMoves();
+
+            if (maximizingPlayer)
+            {
+                int maxEval = int.MinValue;
+                foreach (Move move in allMoves)
+                {
+                    board.MakeMove(move);
+                    int eval = MinMax(board, depth - 1, false, alpha, beta);
+                    board.UndoMove(move);
+                    maxEval = Math.Max(maxEval, eval);
+                    alpha = Math.Max(alpha, eval);
+                    if (beta <= alpha)
+                    {
+                        break;
+                    }
+                }
+
+                return maxEval;
+            }
+            else
+            {
+                int minEval = int.MaxValue;
+                foreach (Move move in allMoves)
+                {
+                    board.MakeMove(move);
+                    int eval = MinMax(board, depth - 1, true, alpha, beta);
+                    board.UndoMove(move);
+                    minEval = Math.Min(minEval, eval);
+                    beta = Math.Min(beta, eval);
+                    if (beta <= alpha)
+                    {
+                        break;
+                    }
+                }
+
+                return minEval;
+            }
+        }
+
+        int GetBoardScore(Board board)
+        {
+            int boardScore = 0;
+            for (int file = 0; file < 8; file++)
+            {
+                for (int rank = 0; rank < 8; rank++)
+                {
+                    Square square = new(file, rank);
+                    Piece piece = board.GetPiece(square);
+                    if (piece != null)
+                    {
+                        int pieceValue = pieceValues[(int)piece.PieceType];
+                        if (piece.IsWhite == board.IsWhiteToMove)
+                        {
+                            boardScore += pieceValue;
+                        }
+                        else
+                        {
+                            boardScore -= pieceValue;
+                        }
+                    }
+                }
+            }
+
+            return boardScore;
         }
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,280 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
+    int[] pieceValues = { 0, 100, 325, 325, 550, 1000, 50000 };
+    int[] victimScore = { 0, 100, 200, 300, 400, 500, 600 };
+
+    static int maxDepth = 3;
+    static uint PVTableSize = 100000;
+    KeyValuePair<ulong, Move>[] PVTable = new KeyValuePair<ulong, Move>[PVTableSize + 2];
+    Move[] PVArray = new Move[maxDepth];
+
+    int[,] searchHistory = new int[13, 64];
+    int[,] searchKillers = new int[2, maxDepth];
+    int searchPly = 0;
+    int searchNodes = 0;
+
+    int[,] MvvLvaScores = new int[13, 13];
+
+    List<ulong> history = new();
+
+    public MyBot()
+    {
+        InitMvvLva();
+    }
+
     public Move Think(Board board, Timer timer)
     {
+        history.Add(board.ZobristKey);
+        Move bestMove = SearchPosition(board, timer);
+
+        board.MakeMove(bestMove);
+        history.Add(board.ZobristKey);
+
+        return bestMove;
+    }
+
+    void InitMvvLva()
+    {
+        for (int attacker = (int)PieceType.Pawn; attacker <= (int)PieceType.King; attacker++)
+            for (int victim = (int)PieceType.Pawn; victim <= (int)PieceType.King; victim++)
+                MvvLvaScores[victim, attacker] = victimScore[victim] + 6 - (victimScore[attacker] / 100);
+    }
+
+    void PickNextMove(int moveNum, ref KeyValuePair<Move, int>[] moveListScores)
+    {
+        int bestScore = 0;
+        int bestIndex = 0;
+
+        for (int i = moveNum; i < moveListScores.Length; i++)
+        {
+            if (moveListScores[i].Value > bestScore)
+            {
+                bestScore = moveListScores[i].Value;
+                bestIndex = i;
+            }
+        }
+
+        KeyValuePair<Move, int> temp = moveListScores[moveNum];
+        moveListScores[moveNum] = moveListScores[bestIndex];
+        moveListScores[bestIndex] = temp;
+    }
+
+    bool IsRepetition(Board board)
+    {
+        for (int i = 0; i < history.Count - 1; i++)
+        {
+            if (history[i] == board.ZobristKey)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void ClearForSearch(Board board)
+    {
+        for (int i = 0; i < 13; i++)
+        {
+            for (int j = 0; j < 64; j++)
+            {
+                searchHistory[i, j] = 0;
+            }
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < maxDepth; j++)
+            {
+                searchKillers[i, j] = 0;
+            }
+        }
+
+        PVTable = new KeyValuePair<ulong, Move>[PVTableSize + 2];
+        searchPly = 0;
+        searchNodes = 0;
+    }
+
+    void StorePVMove(Board board, Move move)
+    {
+        uint index = (uint)board.ZobristKey % PVTableSize;
+        PVTable[index] = new KeyValuePair<ulong, Move>(board.ZobristKey, move);
+    }
+
+    Move ProbePVTable(Board board)
+    {
+        uint index = (uint)board.ZobristKey % PVTableSize;
+        if (PVTable[index].Key == board.ZobristKey)
+        {
+            return PVTable[index].Value;
+        }
+
+        return Move.NullMove;
+    }
+
+    int GetPVLine(Board board, int depth)
+    {
+        Move move = ProbePVTable(board);
+        List<Move> movesToUndo = new();
+        int count = 0;
+
+        while (move != Move.NullMove && count < depth)
+        {
+            if (board.GetLegalMoves().Contains(move))
+            {
+                movesToUndo.Insert(0, move);
+                board.MakeMove(move);
+                PVArray[count++] = move;
+            }
+            else
+            {
+                break;
+            }
+
+            move = ProbePVTable(board);
+        }
+
+        foreach (Move moveToUndo in movesToUndo)
+        {
+            board.UndoMove(moveToUndo);
+        }
+
+        return count;
+    }
+
+    Move SearchPosition(Board board, Timer timer)
+    {
+        Move bestMove = Move.NullMove;
+        int bestScore = int.MinValue;
+        ClearForSearch(board);
+
+        for (int depth = 1; depth <= maxDepth; depth++)
+        {
+            bestScore = AlphaBeta(-9999999, 99999999, depth, board, timer);
+            int pvMoves = GetPVLine(board, depth);
+            bestMove = PVArray[0];
+
+            Console.WriteLine($"[SearchPosition] Depth: {depth} Score: {bestScore} Move: {bestMove} Nodes: {searchNodes}");
+            Console.Write($" > PV: ");
+            for (int i = 0; i < pvMoves; i++)
+            {
+                Console.Write($"{PVArray[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        return bestMove;
+    }
+
+    int AlphaBeta(int alpha, int beta, int depth, Board board, Timer timer)
+    {
+        searchNodes++;
+
+        if (depth == 0 || timer.MillisecondsElapsedThisTurn >= 1000)
+            return GetBoardScore(board);
+
+        if (IsRepetition(board))
+            return 0;
+
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        int oldAlpha = alpha;
+        Move bestMove = Move.NullMove;
+
+        KeyValuePair<Move, int>[] moveListScores = new KeyValuePair<Move, int>[moves.Length];
+        for (int i = 0; i < moves.Length; i++)
+        {
+            Move move = moves[i];
+            int score = move.IsCapture ? MvvLvaScores[(int)move.CapturePieceType, (int)move.MovePieceType] : 0;
+            moveListScores[i] = new KeyValuePair<Move, int>(move, score);
+        }
+
+        if (moves.Length == 0)
+        {
+            return board.IsInCheck() ? -100000 - depth : 0;
+        }
+
+        for (int i = 0; i < moveListScores.Length; i++)
+        {
+            PickNextMove(i, ref moveListScores);
+            Move move = moveListScores[i].Key;
+
+            board.MakeMove(move);
+            int score = -AlphaBeta(-beta, -alpha, depth - 1, board, timer);
+            board.UndoMove(move);
+
+            if (score > alpha)
+            {
+                if (score >= beta)
+                {
+                    return beta;
+                }
+                alpha = score;
+                bestMove = move;
+            }
+        }
+
+        if (alpha != oldAlpha)
+        {
+            StorePVMove(board, bestMove);
+        }
+
+        return alpha;
+    }
+
+    int GetBoardScore(Board board)
+    {
+        int score = 0;
+        for (int file = 0; file < 8; file++)
+        {
+            for (int rank = 0; rank < 8; rank++)
+            {
+                Square square = new(file, rank);
+                Piece piece = board.GetPiece(square);
+                if (piece != null)
+                {
+                    int pieceValue = pieceValues[(int)piece.PieceType] + PieceEvaluation(piece, square);
+                    score += pieceValue * (piece.IsWhite ? 1 : -1);
+                }
+            }
+        }
+
+        return score * (board.IsWhiteToMove ? 1 : -1);
+    }
+
+    int PieceEvaluation(Piece piece, Square square)
+    {
+        int distanceFromCenterX = (int)Math.Abs(square.File - 3.5);
+        int distanceFromCenterY = (int)Math.Abs(square.Rank - 3.5);
+        int distanceFromBackrank = piece.IsWhite ? square.Rank : 7 - square.Rank;
+        int distanceFromCenter = distanceFromCenterX + distanceFromCenterY;
+
+        if (piece.IsKing)
+        {
+            return distanceFromBackrank == 7 ? 0 : -70;
+        }
+        else if (piece.IsQueen)
+        {
+            return 0;
+        }
+        else if (piece.IsRook)
+        {
+            return distanceFromBackrank == 1 ? 25 : Math.Max(0, 10 - distanceFromCenter * 5);
+        }
+        else if (piece.IsBishop || piece.IsKnight)
+        {
+            return Math.Max(0, 20 - distanceFromCenter * 5);
+        }
+        else if (piece.IsPawn)
+        {
+            int pawnDistance = distanceFromBackrank + distanceFromCenterX;
+            return distanceFromBackrank <= 5 ? Math.Max(0, 20 - pawnDistance * 5) : 0;
+        }
+
+        return 0;
     }
 }
